679b1e56a36e2ba981feb0057615f96342d2d809
thiagomendes <Gpower12*>
2022-03-30 15:55:04 -0300

    checkpoint 7 - diagonal rodape para esqueda

---
+++ src/main/java/br/com/mutant/checker/component/DiagonalBottomToLeftPositionMapper.java
@@ -0,0 +1,28 @@
+package br.com.mutant.checker.component;
+
+import br.com.mutant.checker.domain.vo.Position;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DiagonalBottomToLeftPositionMapper implements PositionMapper {
+    @Override
+    public List<List<Position>> getPositions(char[][] matrix, int detectionNumber) {
+        List<List<Position>> root = new ArrayList<>();
+        for (int p0 = 0; p0 < matrix.length; p0++) {
+            List<Position> positions = new ArrayList<>();
+            for (int p1 = matrix.length - 1; p1 >= 0; p1--) {
+                int n = p1 - p0;
+                if (n >= 0) {
+                    positions.add(new Position(p1, n));
+                }
+            }
+            if (positions.size() >= detectionNumber) {
+                root.add(positions);
+            }
+        }
+        return root;
+    }
+}
