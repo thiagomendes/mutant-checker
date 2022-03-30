ab1c180cfa3e0e8a4a48a83555bd81eeb4e1fd25
thiagomendes <Gpower12*>
2022-03-29 21:29:21 -0300

    checkpoint 3 - api documentation ok

--- src/main/java/br/com/mutant/checker/controller/MutantCheckerController.java
+++ src/main/java/br/com/mutant/checker/controller/MutantCheckerController.java
@@ -2,6 +2,9 @@ package br.com.mutant.checker.controller;
 
 import br.com.mutant.checker.dto.DnaCheckerRequestDto;
 import br.com.mutant.checker.service.MutantCheckerService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +22,19 @@ public class MutantCheckerController {
         this.mutantCheckerService = mutantCheckerService;
     }
 
+    @ApiOperation(value = "Based on DNA, checks if it is mutant")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "It's mutant"),
+            @ApiResponse(code = 403, message = "It's not mutant"),
+    })
     @PostMapping("/mutant")
     public ResponseEntity<Void> isMutant(@RequestBody DnaCheckerRequestDto dnaCheckerRequestDto) {
         mutantCheckerService.validateRequest(dnaCheckerRequestDto);
 
         if (mutantCheckerService.isMutant(dnaCheckerRequestDto.getDna())) {
-            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
 }
