0db1a50e127e9312125c0a05d5a0c51d4609c589
thiagomendes <Gpower12*>
2022-03-31 22:25:42 -0300

    adicionando classe de testes para service stats, sem os testes implementados

---
+++ src/test/java/br/com/mutant/checker/service/MutantStatsServiceImplTest.java
@@ -0,0 +1,18 @@
+package br.com.mutant.checker.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class MutantStatsServiceImplTest {
+
+    @Test
+    void testRatio() {
+        Assertions.assertTrue(true);
+        // TODO implementar testes
+    }
+}
